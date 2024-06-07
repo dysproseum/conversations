3b16d48a1d4d74f517febbc387d1ac60ba68d066
dysproseum <dysproseum@gmail.com>
2024-06-07 15:26:15 +0000

    Report on stray data

--- reports.php
+++ reports.php
@@ -18,7 +18,7 @@
       $header = getHeader($user);
       $sidebar = getSidebar($user, "reports");
       $sidebar2 = getSidebar2($user);
-      $content = printReport();
+      $content = generateReport();
   }
 ?>
 
@@ -26,16 +26,36 @@
 <head>
 <?php print $head; ?>
 </head>
-<body class="post">
+<body class="reports">
   <?php print $header; ?>
   <div class="wrapper">
     <?php print $sidebar; ?>
     <div id="content">
-
       <h1 id="contentheader">System Reports</h1>
+
       Total Posts: <?php print $content['num_posts']; ?><br>
       Total Comments: <?php print $content['num_comments']; ?><br>
       Total Users: <?php print $content['num_users']; ?><br>
+      <br>
+      Abandoned Posts: <?php print sizeof($content['posts_no_access']); ?><br>
+      <pre>
+      <?php foreach ($content['posts_no_access'] as $na): ?>
+        <?php print $na . PHP_EOL; ?>
+      <?php endforeach; ?>
+      </pre>
+      Stray Access: <?php print sizeof($content['access_no_posts']); ?><br>
+      <pre>
+      <?php foreach ($content['access_no_posts'] as $na): ?>
+        <?php print $na . PHP_EOL; ?>
+      <?php endforeach; ?>
+      </pre>
+      Stray Comments: <?php print sizeof($content['comment_no_posts']); ?><br>
+      <pre>
+      <?php foreach ($content['comment_no_posts'] as $na): ?>
+        <?php print $na . PHP_EOL; ?>
+      <?php endforeach; ?>
+      </pre>
+
     </div>
     <?php print $sidebar2; ?>
   </div>
